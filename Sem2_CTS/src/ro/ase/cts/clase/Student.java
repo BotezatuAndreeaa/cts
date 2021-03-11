cf4f3edb67e7d3692aca8fb757322051a0ec056b
botez <botez@DESKTOP-V6V2FBP>
2021-03-11 11:21:18 +0200

    Seminar 3 - to string, suma finantata

--- Sem2_CTS/src/ro/ase/cts/clase/Student.java
+++ Sem2_CTS/src/ro/ase/cts/clase/Student.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Student extends Aplicant {
 	protected String facultate;
 	protected int an_studii;
+	private static int sumaFinantata=20;
 	
 	
 	public String getFacultate() {
@@ -33,14 +34,22 @@ public class Student extends Aplicant {
 	}
 	@Override
 	public String toString() {
-		return "Student: Nume=" + nume + ", Prenume=" + prenume + ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte +  ", DenumireProiect=" + Arrays.toString(denumireProiect) + "Facultate=" + facultate + ", An_studii=" + an_studii ;
+		return "Student:" +super.toString()+ "Facultate=" + facultate + ", An_studii=" + an_studii +"]";
 	}
 	
 	
-	public int finantare() {
-		int s=20;
-		System.out.println("Studentul "+getNume()+" "+getPrenume()+" primeste"+s+" Euro/zi in proiect.");
-		return s;
+
+	@Override
+	public void afiseazaSumaFinantata() {
+		// TODO Auto-generated method stub
+		System.out.println("Studentul "+getNume()+" "+getPrenume()+" primeste "+sumaFinantata+" Euro/zi in proiect.");
+		
+	}
+	public static int getSumaFinantata() {
+		return sumaFinantata;
+	}
+	public static void setSumaFinantata(int sumaFinantata) {
+		Student.sumaFinantata = sumaFinantata;
 	}
 	
 }
--- Sem2_CTS/src/ro/ase/cts/program/Program.java
+++ Sem2_CTS/src/ro/ase/cts/program/Program.java
@@ -1,35 +1,40 @@
 package ro.ase.cts.program;
 
 
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import readers.AngajatReader;
+
 import readers.AplicantReader;
 import readers.StudentReader;
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Student;
 
 public class Program {
 	
 	//diferenta intre functii si metode: metoda apartine unui ob ->ptr this
 	//pt metode si functii ->verbe
 	
-	public static List<Aplicant> readAplicant(String fisier, StudentReader studentReader) throws NumberFormatException,FileNotFoundException {
+	public static List<Aplicant> readAplicant( AplicantReader reader) throws NumberFormatException,FileNotFoundException {
 		//principiul liskov: implemnetarea astfel incat daca inlocuim cu orice ob al clasei fiu sa avem aceasi implementare
-	return studentReader.readAplicants();
+	return reader.readAplicants();
 	
 	}
 
 	public static void main(String[] args) {
 		//nu trebuie sa depindem in implementari de clasele concrete, ci de cele abstracte 
 		//clasa aplicand este abstracta
-		
+		System.out.println("Suma finantata student: "+Student.getSumaFinantata());
+		System.out.println("Suma finantata ngajat:" +Angajat.getSumaFinantata());
 		List<Aplicant> listaAplicanti;
 		try {
-			listaAplicanti=readAplicants(new StudentReader("studenti.txt"));
-			for(Aplicant aplicant:listaAplicanti)
+			listaAplicanti=readAplicant(new StudentReader("studenti.txt"));
+			for(Aplicant aplicant:listaAplicanti) {
 				System.out.println(aplicant.toString());
+				aplicant.afiseazaSumaFinantata();
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,5 +42,6 @@ public class Program {
 		
 		
 	}
+
 
 }
